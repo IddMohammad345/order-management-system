6090b82a0a9d0b8a40f1de091e471f78b4c7b8ff
Idd mohammad <mohammadidd345@gmail.com>
2026-08-25 15:50:35 +0530

    implemented service discovery

--- order-Service/src/main/java/com/idd/orderservice/client/ProductClient.java
+++ order-Service/src/main/java/com/idd/orderservice/client/ProductClient.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-Service",
-        url = "${product.service.url}")
+@FeignClient(name = "product-service")
 public interface ProductClient {
     @GetMapping("/api/v1/products/{id}")
     ProductResponse getProduct(
--- order-Service/src/main/java/com/idd/orderservice/client/UserClient.java
+++ order-Service/src/main/java/com/idd/orderservice/client/UserClient.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-Service",
-        url = "${user.service.url}")
+@FeignClient(name = "user-service")
 public interface UserClient {
     @GetMapping("/api/v1/users/{id}")
     UserResponse getUser(
